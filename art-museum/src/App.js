aa54601cae6522778cf4c253a0224bf8ddb72c90
TheNerdyWyvern <raynorhoobler@gmail.com>
2023-05-23 17:00:07 -0700

    phase 2 started

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
+import GalleryView from "./components/GalleryView";
+import { Route, useParams } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt.records}/>
+      <Route path="/galleries/:galleryId" component={GalleryView}/>
     </div>
   );
 }
---
+++ art-museum/src/components/GalleryView/index.js
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+{ useParams}
+
+export default function GalleryView() {
+    return (
+        <h1>Hello from GalleryView</h1>
+    );
+}
